dd830cfd482267d5c5af6f08a01f65de8af662ad
rewin <rewin@DESKTOP-TL232GJ>
2021-03-20 20:41:26 +0300

    Add character

---
+++ MainCharacter.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+const GRAVITY = 200.0
+const WALK_SPEED = 200
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+
+	move_and_slide(velocity, Vector2(0, -1))
+
