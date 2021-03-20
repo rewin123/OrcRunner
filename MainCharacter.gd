9f9ce29652a123e6392b29def56d5f63b23448c1
Kirito <rewin1996@gmail.com>
2021-03-20 21:22:24 +0300

    player

--- MainCharacter.gd
+++ MainCharacter.gd
@@ -10,20 +10,53 @@ extends KinematicBody2D
 func _ready():
 	pass # Replace with function body.
 
-const GRAVITY = 200.0
-const WALK_SPEED = 200
+const GRAVITY = 400.0
+const WALK_SPEED = 100
+const RUN_SPEED = 200
+const jump = -100
+const stop = 10
+const acc = 50
 
-var velocity = Vector2()
+onready var ip = get_node("AnimatedSprite")
+
+var vec = Vector2()
+var walk = 0
 
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
-
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  WALK_SPEED
+	vec.y += delta * GRAVITY
+	if Input.is_action_pressed("shift"):
+		walk = 1
 	else:
-		velocity.x = 0
+		walk = 0
+	
+	
+	if walk == 0:
+		if Input.is_action_pressed("A") and vec.x > -WALK_SPEED:
+			vec.x -= acc
+			ip.flip_h = true
+		if Input.is_action_pressed("D") and vec.x < WALK_SPEED:
+			vec.x += acc
+			ip.flip_h = false
+	else:
+		if Input.is_action_pressed("A") and vec.x > -RUN_SPEED:
+			vec.x -= acc
+			ip.flip_h = true
+		if Input.is_action_pressed("D") and vec.x < RUN_SPEED:
+			vec.x += acc
+			ip.flip_h = false
+	
+	
+	
+	
+	if vec.x > -10 and vec.x < 10:
+		vec.x = 0
+	elif vec.x > 0:
+		vec.x -= stop
+	elif vec.x < 0:
+		vec.x += stop
+	
+	if Input.is_action_pressed("space") and vec.y > 1:
+		vec.y = jump
 
-	move_and_slide(velocity, Vector2(0, -1))
+	move_and_slide(vec, Vector2(0, -1))
 
