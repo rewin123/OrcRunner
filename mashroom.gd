f6fb4becef9fe9f1d0fbad9d43e287f4e6470c11
Kirito <rewin1996@gmail.com>
2021-03-21 11:48:55 +0300

    hunger

---
+++ mashroom.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var sp = get_node("Sprite2")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	if $mushroom.position == $player.position :
+		sp.Visible = true
+	else:
+		sp.Visible = false
+	
+	
+	pass
--- props/mushroom/mushroom.gd
+++ props/mushroom/mushroom.gd
@@ -5,12 +5,22 @@ extends Area2D
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	
+	pass
+
+
+func _on_Mushroom_area_entered(area):
+	$Sprite2.Visible = true
+	pass
+
+
+func _on_Mushroom_area_exited(area):
+	$Sprite2.Visible = false
+	pass
